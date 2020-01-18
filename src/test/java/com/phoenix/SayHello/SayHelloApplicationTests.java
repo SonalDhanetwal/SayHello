458dac4063eb7790c0991e4886511d220ebf2321
Nitin Rai <nitinrai17@gmail.com>
2020-01-18 10:05:01 +0530

    1st commit

---
+++ src/test/java/com/phoenix/SayHello/SayHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.SayHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
